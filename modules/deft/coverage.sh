6abaf073cab17368db2340a1f3a7eb62985edee7
Yuqi Huai <yhuai@uci.edu>
2025-02-11 20:47:39 -0800

    impl: deft genhtml use $USER path

--- modules/deft/coverage.sh
+++ modules/deft/coverage.sh
@@ -1,3 +1,4 @@
 source /apollo/scripts/apollo_base.sh
 bazel coverage -s --combined_report=lcov --instrumentation_filter="^//modules/planning[/:]" //modules/deft:main_test
-genhtml --output /apollo/modules/deft/genhtml "$(bazel info output_path)/_coverage/_coverage_report.dat"
+rm -rf /home/$USER/deft/genhtml
+genhtml --output /home/$USER/deft/genhtml "$(bazel info output_path)/_coverage/_coverage_report.dat"
