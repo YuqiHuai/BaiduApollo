da84b65529f0ebf8008e215d86f47b450f324b92
Yuqi Huai <yhuai@uci.edu>
2025-03-04 23:44:08 -0800

    fix: deft uses batch mode

--- modules/deft/coverage.sh
+++ modules/deft/coverage.sh
@@ -1,4 +1,4 @@
 source /apollo/scripts/apollo_base.sh
-bazel coverage -s --combined_report=lcov --instrumentation_filter="^//modules/planning[/:]" //modules/deft:main_test
+bazel coverage --batch -s --combined_report=lcov --instrumentation_filter="^//modules/planning[/:]" //modules/deft:main_test
 rm -rf /home/$USER/deft/genhtml
 genhtml --output /home/$USER/deft/genhtml "$(bazel info output_path)/_coverage/_coverage_report.dat"
